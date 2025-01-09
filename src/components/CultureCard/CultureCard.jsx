18cd34c35463c9f1701d56e7ee47ecb75dc61e14
Your Name <you@example.com>
2025-01-09 18:47:01 -0500

    Build CultureCards Section, CultureCard Component

--- src/components/CultureCard/CultureCard.jsx
+++ src/components/CultureCard/CultureCard.jsx
@@ -1,13 +1,12 @@
 // --------- STYLES ---------
 import './CultureCard.scss'
 
-export default function CultureCard() {
-
+export default function CultureCard({ blurb }) {
 
     return (
         <div className="card">
-            <h3 className="card-title"></h3>
-            <p className="card-text"></p>
+            <h3 className="card-title">{blurb.title}</h3>
+            <p className="card-text">{blurb.text}</p>
         </div>
     );
 }
--- src/components/CultureCards/CultureCards.jsx
+++ src/components/CultureCards/CultureCards.jsx
@@ -1,3 +1,5 @@
+// ---------- FXNALITY ----------
+import blurb from "../../data/culturecards.json";
 // --------- COMPONENTS ---------
 import CultureCard from '../CultureCard/CultureCard';
 // --------- STYLES ---------
@@ -5,10 +7,19 @@ import './CultureCards.scss'
 
 export default function CultureCards() {
 
+    let cardText = blurb;
 
     return (
         <section className="culture">
-            <CultureCard />
+            <h2 className="culture__title">Driven by a century of culture</h2>
+            {cardText.map((blurb) => {
+                return <CultureCard key={blurb.id} blurb={blurb} />
+            })}
+            <h4 className="culture__legal">® Registered trademark of The Bank of Nova Scotia, used under licence. Scotia Wealth Management® consists of a range of financial services provided by The Bank of Nova Scotia (Scotiabank®); The Bank of Nova Scotia Trust Company (Scotiatrust®); Private Investment Counsel, a service of 1832 Asset Management L.P.; 1832 Asset Management U.S. Inc.; Scotia Wealth Insurance Services Inc.; and ScotiaMcLeod®, a division of Scotia Capital Inc. Private banking services are provided by The Bank of Nova Scotia. Estate and trust services are provided by The Bank of Nova Scotia Trust Company. Portfolio management is provided by 1832 Asset Management L.P. and 1832 Asset Management U.S. Inc. Insurance services are provided by Scotia Wealth Insurance Services Inc. Wealth advisory and brokerage services are provided by ScotiaMcLeod, a division of Scotia Capital Inc. International investment advisory services are provided by Scotia Capital Inc. Financial planning services are provided by The Bank of Nova Scotia and ScotiaMcLeod. Scotia Capital Inc. is a member of the Canadian Investor Protection Fund and is regulated by the Canadian Investment Regulatory Organization. Scotia Wealth Insurance Services Inc. is the insurance subsidiary of Scotia Capital Inc., a member of the Scotiabank group of companies. When discussing life insurance products, ScotiaMcLeod advisors are acting as Life Insurance Agents (Financial Security Advisors in Quebec) representing Scotia Wealth Insurance Services Inc.
+            </h4>
+            <img src="src/assets/images/CIPF.avif" alt="CIPF Member Logo" className="culture__logo CIPF" />
+            <img src="src/assets/images/CIRO.avif" alt="Regulated by CIRO Logo" className="culture__logo CIRO" />
+            <img src="src/assets/images/IIROC.avif" alt="IIROC AdvisorReport Logo" className="culture__logo IIROC" />
         </section>
     );
 }
