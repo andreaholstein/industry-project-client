2f3cf5123cec8d95b5f12ebcb151a3cddfafe055
Your Name <you@example.com>
2025-01-09 14:16:41 -0500

    Initialize Project - Client-side

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// --------- FXNALITY ---------
+// import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// --------- PAGES ---------
+// import HomePage from './Pages/HomePage/HomePage.jsx';
+// --------- COMPONENTS ---------
+// import Header from "./components/Header/Header.jsx";
+// import Footer from "./components/Footer/Footer.jsx";
+// --------- STYLES ---------
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      {/* <Header /> */}
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<HomePage />} /> */}
+        </Routes>
+      </BrowserRouter>
+      {/* <Footer /> */}
+    </>
+  )
+}
+
+export default App
