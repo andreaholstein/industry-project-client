45bb7d1c86e556af8817d007fb2fa70b4ae84ed3
Kaley S. Jeon <kaleys.jeon@Kaleys-iMac.local>
2025-01-10 10:26:29 -0500

    Finish creating carousel 2 card. Revise json and jsx file to initialize the json. Add the component in the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Footer from "./components/Footer/Footer.jsx";
 import CultureCards from "./components/CultureCards/CultureCards.jsx"
 // --------- STYLES ---------
 import './App.scss'
+import Carousel2Card from './components/Carousel2Card/Carousel2Card.jsx';
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
       {/* <BrowserRouter> */}
       {/* <Routes> */}
       {/* <Route path="/" element={<HomePage />} /> */}
+      <Carousel2Card />
       <CultureCards />
       {/* </Routes> */}
       {/* </BrowserRouter> */}
