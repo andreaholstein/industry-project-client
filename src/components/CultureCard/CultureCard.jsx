e8365e5b9d5c11ae609cb6e825924b7ab8dd4965
Your Name <you@example.com>
2025-01-10 11:25:25 -0500

    Fix Footer SVGs, Tidy CultureCard/s Styles

--- src/components/CultureCard/CultureCard.jsx
+++ src/components/CultureCard/CultureCard.jsx
@@ -7,7 +7,28 @@ export default function CultureCard({ blurb }) {
         <div className="card">
             <div className="card__red-line"></div>
             <h3 className="card__title">{blurb.title}</h3>
-            <p className="card__text">{blurb.text}</p>
+            <div className="card__body">
+                {blurb.text.map((text, index) => (
+                    <p className="card__text" key={index}>{text}</p>
+                    // <br></br>
+                ))}
+            </div>
+
         </div>
     );
 }
+{/* <p className="card__text">{blurb.text[0]}</p>
+<br></br>
+<p className="card__text">{blurb.text[1]}</p>
+<br></br>
+<p className="card__text">{blurb.text[2]}</p>
+<br></br>
+<p className="card__text">{blurb.text[3]}</p>
+<br></br>
+<p className="card__text">{blurb.text[4]}</p>
+<br></br>
+<p className="card__text">{blurb.text[5]}</p>
+<br></br>
+<p className="card__text">{blurb.text[6]}</p>
+<br></br>
+<p className="card__text">{blurb.text[7]}</p> */}
