87133e97ebd37d20c71bdd67e3d510b9f27eef20
Lospub <zhjyy002@gmail.com>
2025-01-10 09:52:41 -0700

    create contact component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import "./Contact.scss"
+import ContactImage from "../../assets/images/contact.png"
+
+const Contact = () => {
+    return (
+        <>
+        </>
+        );
+    };
+    
+    export default Contact;
