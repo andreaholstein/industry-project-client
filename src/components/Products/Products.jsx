f0fa142f0ff0c108334ca14e7fea09f3ec67576a
Elizabeth Ko <elizabeth_ko7@hotmail.com>
2025-01-09 20:01:18 -0800

    added products.jsx

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,13 @@
+import "./Products.scss";
+
+export default function Products() {
+  return (
+    <div className="card">
+      <div className="tab-container"></div>
+      <div className="main-container">
+        <h1 className="main__title"></h1>
+        <p className="main__paragraph"></p>
+      </div>
+    </div>
+  );
+}
